8fddb94b80328b380af03a1eb13a5172a6079ed4
Sunny <sunnylee945@hotmail.com>
2017-10-16 17:18:11 +0800

    add DesignerOverViewController file

---
+++ StaytunediOS/DesignerOverview/DesignerOverviewViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DesignerOverviewViewController.swift
+//  StaytunediOS
+//
+//  Created by Mac on 2017/10/16.
+//  Copyright © 2017年 Staytuned.Design. All rights reserved.
+//
+
+import UIKit
+
+public class DesignerOverviewViewController: UIViewController {
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  public override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
